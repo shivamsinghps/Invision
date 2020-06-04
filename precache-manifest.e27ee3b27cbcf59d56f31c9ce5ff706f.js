7b998ed7db2e3575f6a0cd285d3a2627df4303dc
shivamsinghps <shivamsinghps@gmail.com>
2020-06-04 14:31:19 +0530

    Updates

---
+++ precache-manifest.e27ee3b27cbcf59d56f31c9ce5ff706f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df514dfb52969c28b252caf8b4981355",
+    "url": "/Invision/index.html"
+  },
+  {
+    "revision": "6d4f335215c678b9ef50",
+    "url": "/Invision/static/css/main.5cf90a56.chunk.css"
+  },
+  {
+    "revision": "8566cb854e1487ec00da",
+    "url": "/Invision/static/js/2.5bddc4b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Invision/static/js/2.5bddc4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4f335215c678b9ef50",
+    "url": "/Invision/static/js/main.39fad595.chunk.js"
+  },
+  {
+    "revision": "e60e433cae89796dacec",
+    "url": "/Invision/static/js/runtime-main.b1dc85de.js"
+  },
+  {
+    "revision": "0afc125113adf53b55d87f2eb68ced93",
+    "url": "/Invision/static/media/Header.0afc1251.jpg"
+  },
+  {
+    "revision": "82189696539164bc0b0e271a3bcac397",
+    "url": "/Invision/static/media/envelope-solid.82189696.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Invision/precache-manifest.e27ee3b27cbcf59d56f31c9ce5ff706f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Invision/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
